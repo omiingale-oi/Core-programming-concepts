c4c4561c7c42df2e4bddecea753f2cb4995a08d7
Om Ingale <omcingale@gmail.com>
2026-01-03 07:18:48 +0530

    Added basic variables and data types example

---
+++ basics/variables_and_types.py
@@ -0,0 +1,11 @@
+# Core programming concept: variables and data types
+
+age = 19          # integer
+name = "Omii"     # string
+height = 170.5    # float
+is_student = True # boolean
+
+print(age)
+print(name)
+print(height)
+print(is_student)
